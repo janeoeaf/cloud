d1f79eb5919a6588840c48384fe06f9c408bf24a
janeoeaf <janeoeaf@gmail.com>
2021-05-09 03:46:49 +0000

    take repos

--- take_repos.sh
+++ take_repos.sh
@@ -4,4 +4,6 @@ repos_path=/repos/
 
 mkdir -p $repos_path
 
-git clone https://
+git clone https://github.com/janeoeaf/cloud.git .
+git config --global user.email "janeoeaf@gmail.com"
+git config --global user.name "janeoeaf"
