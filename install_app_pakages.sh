c77bc632662e562bb265e6fd937e38bfd831691f
janeoeaf <janeoeaf@gmail.com>
2021-05-09 04:02:29 +0000

    pakages

---
+++ install_app_pakages.sh
@@ -0,0 +1,10 @@
+sudo apt-get install python3
+sudo apt-get install r-base
+sudo apt-get install wget
+
+sudo apt-get install gdebi-core libapparmor1
+wget http://download2.rstudio.org/rstudio-server-0.98.1091-amd64.deb
+sudo gdebi rstudio-server-0.98.1091-amd64.deb
+
+
+
